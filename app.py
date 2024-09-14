9108370dee096d492952db97fd6441045d4eb182
Wojciech Łapacz <wojciech.lapacz02@gmail.com>
2024-09-14 15:57:55 +0200

    update

--- app.py
+++ app.py
@@ -7,51 +7,28 @@ import pandas as pd
 from data_retrieval.loader import load_local, get_rgb_images
 
 
-def load_images_for_coords(lat, lon):
+def get_images(lat, lon):
     ds = load_local()
     years, images = get_rgb_images(ds)
     return images[0], images[1]
 
 
-def get_data(lat, lon):
-    pass
-
-
-def show_images_comparison(data):
+def show_images_comparison(data, show_ndvi=False):
     lat, lon = data["coords"]
-    img1, img2 = load_images_for_coords(lat, lon)
+    img1, img2 = get_images(lat, lon)
     col1, col2 = st.columns(2)
     with col1:
         st.subheader("Title 1")
-        st.image(img1, caption="Image 1", use_column_width=True, clamp=True)
+        st.image(img1, caption="Image 1", use_column_width=True)
     with col2:
         st.subheader("Title 2")
-        st.image(img2, caption="Image 2", use_column_width=True, clamp=True)
-
-    plot_data = get_data(lat, lon)
-
+        st.image(img2, caption="Image 2", use_column_width=True)
+    
     area_data = pd.DataFrame(np.random.randn(20, 1), columns=["Forest Area"])
     st.area_chart(area_data)
 
     line_data = pd.DataFrame(np.random.randn(20, 2), columns=["Temperature", "CO2"])
-    st.line_chart(line_data, x="Year")
-
-    quality_data = pd.DataFrame(
-        np.random.randn(20, 1),
-        columns=[
-            "nvdi",
-        ],
-    )
-    st.line_chart(quality_data, x="Year")
-
-
-# def get_important_points():
-#     points = [
-#         {'lat': 51.1074, 'lon': 17.0382, 'name': 'Wroclaw'},
-#         {'lat': 52.2298, 'lon': 21.0118, 'name': 'Warsaw'},
-#         {'lat': 50.0647, 'lon': 19.9450, 'name': 'Cracow'}
-#     ]
-#     return points
+    st.line_chart(line_data)
 
 
 def main():
@@ -75,13 +52,13 @@ def main():
     if map.get("last_clicked"):
         point = (map["last_clicked"]["lat"], map["last_clicked"]["lng"])
 
-    show_images = st.button("Show/Hide Images")
 
-    if not show_images and point is not None:
+    if point is not None:
+        show_ndvi = st.button("Show NDVI")
         data = {
             "coords": point,
         }
-        show_images_comparison(data)
+        show_images_comparison(data, show_ndvi)
 
 
 if __name__ == "__main__":
