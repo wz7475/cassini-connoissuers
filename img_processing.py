4accec70ed1a0e12a84fc8cc94a3136061b942dd
Jan Fidor <jan.p.fidor@gmail.com>
2024-09-14 15:44:25 +0200

    add filter small islands and parametrize kernels

--- img_processing.py
+++ img_processing.py
@@ -1,11 +1,19 @@
 import numpy as np
 import cv2
 
-def close_mask_clusters(img: np.ndarray, dilation_kernel_size: int = 3, closing_kernel_size: int = 5):
+def close_mask_clusters(
+  img: np.ndarray,
+  dilation_kernel_size: int = 3,
+  opening_kernel_size: int = 5,
+  closing_kernel_size: int = 5,
+  iterations: int = 1
+):
   img = img.astype('uint8')
-  dilation_kernel = np.ones((3, 3), dtype=np.uint8)
-  closing_kernel = np.ones((5, 5), dtype=np.uint8)
-  dilated_mask = cv2.dilate(img, dilation_kernel, iterations=3)
+  dilation_kernel = np.ones((dilation_kernel_size, dilation_kernel_size), dtype=np.uint8)
+  opening_kernel = np.ones((opening_kernel_size, opening_kernel_size), dtype=np.uint8)
+  closing_kernel = np.ones((closing_kernel_size, closing_kernel_size), dtype=np.uint8)
+  dilated_mask = cv2.dilate(img, dilation_kernel, iterations=iterations)
   closed_mask = cv2.morphologyEx(dilated_mask, cv2.MORPH_CLOSE, closing_kernel)
-  eroded_mask = cv2.erode(closed_mask, dilation_kernel, iterations=3)
+  opened_mask = cv2.morphologyEx(closed_mask, cv2.MORPH_OPEN, opening_kernel)
+  eroded_mask = cv2.erode(opened_mask, dilation_kernel, iterations=iterations)
   return eroded_mask
